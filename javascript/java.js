39dafefa13c2a6d2fb85df4f479bb2836c5fe579
Luke Klymshyn <unseenmountain@gmail.com>
2019-11-20 22:48:01 -0800

    I have added my firebase infomation so it can work

---
+++ javascript/java.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCtKZ0nmLMZtdOLHS_M4tLRJcPQFVZ1DeM",
+    authDomain: "train-station-ac729.firebaseapp.com",
+    databaseURL: "https://train-station-ac729.firebaseio.com",
+    projectId: "train-station-ac729",
+    storageBucket: "train-station-ac729.appspot.com",
+    messagingSenderId: "580715683846",
+    appId: "1:580715683846:web:a487f888cd32fdbd30a387",
+    measurementId: "G-VJ0XENCEQD"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
